9250d1bd058fb3ae85af423196deebc8635466d7
Ros-Ma <machacarosana@gmail.com>
2024-11-16 13:52:24 -0300

    agregado la clase8

---
+++ Clase8/4.condicionales.js
@@ -0,0 +1,13 @@
+//consigna
+//crea un programa que simule la calificacion de un estudiante...
+let nota = 55;
+if (nota < 60) {
+    console.log("reprobado"); //si la nota es menor a 60 el alumno a reprobado
+}
+ else if (nota>=60 && nota<=89) {
+
+    console.log("aprobado") //si la nota esta entre 60 y 89 inclusive el alumno a aprobado
+ }
+ else 
+
+ console.log(`excelente calificación`) //si la nota es igual o mayor a 90 obtuvo excelente calificación
