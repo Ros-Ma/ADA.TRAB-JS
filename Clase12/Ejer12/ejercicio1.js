a8180dfd8664f4882f735aa7cf8e8e7156047127
Ros-Ma <machacarosana@gmail.com>
2024-12-04 20:57:11 -0300

    agregar cambios

---
+++ Clase12/Ejer12/ejercicio1.js
@@ -0,0 +1,3 @@
+//Ejercicio 1: While
+//Crea un programa que solicite al usuario ingresar números continuamente hasta que el usuario ingrese un número negativo. Luego, imprime la suma de todos los números ingresados.
+const prompt = require('prompt-sync')();
